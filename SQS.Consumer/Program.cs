b23bc508439f0db9e6d52cdcf41f10e9b275e05a
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-03-26 19:19:31 -0300

    Add SQS provider and consumer

---
+++ SQS.Consumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+var request = new ReceiveMessageRequest
+{
+    QueueUrl = "https://sqs.us-east-1.amazonaws.com/221307536465/teste"
+};
+
+var response = await client.ReceiveMessageAsync(request);
+
+foreach (var msg in response.Messages)
+{
+
+    await client.DeleteMessageAsync("https://sqs.us-east-1.amazonaws.com/221307536465/teste", msg.ReceiptHandle);
+}
---
+++ SQS.Provider/Program.cs
@@ -0,0 +1,12 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+var request = new SendMessageRequest
+{
+    QueueUrl = "https://sqs.us-east-1.amazonaws.com/221307536465/teste",
+    MessageBody = "teste 123"
+};
+
+await client.SendMessageAsync(request);
