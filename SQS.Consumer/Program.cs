79ed6678f2e7ff191336f44bf3fa2c430b7e092d
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-04-01 11:52:53 -0300

    add changes

--- SQS.Consumer/Program.cs
+++ SQS.Consumer/Program.cs
@@ -8,10 +8,13 @@ var request = new ReceiveMessageRequest
     QueueUrl = "https://sqs.us-east-1.amazonaws.com/221307536465/teste"
 };
 
-var response = await client.ReceiveMessageAsync(request);
-
-foreach (var msg in response.Messages)
+while (true)
 {
+    var response = await client.ReceiveMessageAsync(request);
 
-    await client.DeleteMessageAsync("https://sqs.us-east-1.amazonaws.com/221307536465/teste", msg.ReceiptHandle);
+    foreach (var msg in response.Messages)
+    {
+
+        await client.DeleteMessageAsync("https://sqs.us-east-1.amazonaws.com/221307536465/teste", msg.ReceiptHandle);
+    }
 }
